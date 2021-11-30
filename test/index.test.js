e2e9d28c251090753268f9616afffa88ff09ba15
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-11-30 17:28:17 +0530

    Test cases for all Modules.

--- test/index.test.js
+++ test/index.test.js
@@ -10,14 +10,85 @@
 // file. In this case we've defined the function and the corresponding test in the
 // same file for illustrative and learning purposes.
 
-function myCoolFunction() {
-  return 'Wow, what a cool function';
-}
+const { checkIfUndefined, isValidAmount, checkIfValidCurrency } = require('../src/validator-function');
+const { validCurrencies } = require ('../src/validator-function');
 
-describe('myCoolFunction()', () => {
-  test('should return the message: "Wow, what a cool function"', () => {
-    const result = myCoolFunction();
+//Negative test case with empty value to check if undefined
+describe('checkIfUndefined()', () => {
+  test('should return the message: "Invalid Input for amount. Value: "', () => {
+    const result = checkIfUndefined("", "amount");
+    expect(result).toBe("Invalid Input for amount. Value: ");
+  });
+});
 
-    expect(result).toBe('Wow, what a cool function');
+//Positive valid test case to check if undefined
+describe('checkIfUndefined()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = checkIfUndefined(3, "amount");
+    expect(result).toBe(undefined);
+  });
+});
+
+//Negative test case to check if undefined
+describe('checkIfUndefined()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = checkIfUndefined(-3, "amount");
+    expect(result).toBe(undefined);
+  });
+});
+
+//Negative test case to check if undefined
+describe('checkIfUndefined()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = checkIfUndefined("SH", "amount");
+    expect(result).toBe(undefined);
+  });
+});
+
+//InValid Amount value undefined test case
+describe('checkIfUndefined()', () => {
+  test('should return the message: "Invalid Input for amount. Value: undefined"', () => {
+    const result = checkIfUndefined(undefined, "amount");
+    expect(result).toBe("Invalid Input for amount. Value: undefined");
+  });
+});
+
+//InValid Amount value - negative test case
+describe('isValidAmount()', () => {
+  test('should return the message: "Invalid Input, The amount given is less than Zero. Value: -3"', () => {
+    const result = isValidAmount(-3);
+    expect(result).toBe("Invalid Input, The amount given is less than Zero. Value: -3");
+  });
+});
+
+//Valid Amount value - zero test case
+describe('isValidAmount()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = isValidAmount(0);
+    expect(result).toBe(undefined);
+  });
+});
+
+//Valid Amount value test case
+describe('isValidAmount()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = isValidAmount(10);
+    expect(result).toBe(undefined);
+  });
+});
+
+//Valid Supported Currency value test case
+describe('checkIfValidCurrency()', () => {
+  test('should return the message: "undefined"', () => {
+    const result = checkIfValidCurrency("CAD", "Base Currency");
+    expect(result).toBe(undefined);
+  });
+});
+
+//Unsupported Currency value test case
+describe('checkIfValidCurrency()', () => {
+  test('should return the message: "The Base Currency is not a valid Global Currency. Value: CD. The supported currencies are: CAD,USD,GBP,EUR"', () => {
+    const result = checkIfValidCurrency("CD", "Base Currency");
+    expect(result).toBe("The Base Currency is not a valid Global Currency. Value: CD. The supported currencies are: CAD,USD,GBP,EUR");
   });
 });
